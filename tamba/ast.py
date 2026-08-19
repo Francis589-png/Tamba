502ac54994ba2d72ee6e556cac6332045d8c9da7
Francis589-png <jusufrancis08@gmail.com>
2026-08-19 11:06:16 +0100

    Improve AST source locations for diagnostics

--- tamba/ast.py
+++ tamba/ast.py
@@ -1,29 +1,108 @@
 from dataclasses import dataclass
+
+
 @dataclass
-class Program: statements:list
+class Program:
+    statements: list
+
+
 @dataclass
-class Literal: value:object
+class Literal:
+    value: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Variable: name:str
+class Variable:
+    name: str
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Binary: left:object; op:str; right:object
+class Binary:
+    left: object
+    op: str
+    right: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Unary: op:str; expr:object
+class Unary:
+    op: str
+    expr: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Assign: name:str; value:object
+class Assign:
+    name: str
+    value: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Let: name:str; value:object
+class Let:
+    name: str
+    value: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Call: callee:object; args:list
+class Call:
+    callee: object
+    args: list
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class ExprStmt: expr:object
+class ExprStmt:
+    expr: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Block: statements:list
+class Block:
+    statements: list
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class If: condition:object; then:Block; otherwise:object
+class If:
+    condition: object
+    then: Block
+    otherwise: object
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class While: condition:object; body:Block
+class While:
+    condition: object
+    body: Block
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Function: name:str; params:list; body:Block
+class Function:
+    name: str
+    params: list
+    body: Block
+    line: int = 1
+    column: int = 1
+
+
 @dataclass
-class Return: value:object
+class Return:
+    value: object
+    line: int = 1
+    column: int = 1
